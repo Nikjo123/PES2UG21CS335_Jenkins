9d042f6ceba92abd7b40c7e11c90ba23b849855c
Nikjo123 <92309098+Nikjo123@users.noreply.github.com>
2024-03-07 14:52:05 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully changed the file and built and ran\n";
+  cout << "I have successfully changed the file\n";
   return 0;
 }
